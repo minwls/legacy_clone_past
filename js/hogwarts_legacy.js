ad9f838a0e1167113b5edb6924357a5a35861df1
minwls <minyujin0312@gmail.com>
2022-10-28 18:18:44 +0900

    00

--- js/hogwarts_legacy.js
+++ js/hogwarts_legacy.js
@@ -8,5 +8,53 @@ buger.onclick = function(){
    mobile.classList.add('active');
 }
 xx.onclick = function(){
-   mobile.classList.remove('active');
+   mobile.classList.add('off');
+   setTimeout(()=>{
+      mobile.classList.remove('active');
+      mobile.classList.remove('off');
+   },600)
 }
+
+
+
+
+
+function scroll(){
+   
+   const bottomMain = document.querySelector('.bottom-main').offsetTop;
+   const Subcard = document.querySelectorAll('.scroll');
+
+   let sd = {y:0, y2:0, state:true}
+
+   let offset = [];
+
+   Subcard.forEach((ele,k)=>{
+      if(k==0){
+         offset.push( (bottomMain + ele.offsetTop) - window.innerHeight );
+      }else{
+         offset.push( ele.offsetTop - window.innerHeight );
+      }
+   })
+
+   console.log(offset)
+
+   window.addEventListener('scroll',()=>{
+
+      sd.y = window.pageYOffset
+
+      sd.state = (sd.y > sd.y2) ? true : false; 
+
+      sd.y2 = sd.y;
+      
+
+      if(sd.state){
+         Subcard.forEach((ele,idx)=>{
+            if(window.pageYOffset > offset[idx]){
+               Subcard[idx].classList.add('active');
+               }
+         })
+      }
+   });
+}
+
+window.addEventListener('load', scroll)
